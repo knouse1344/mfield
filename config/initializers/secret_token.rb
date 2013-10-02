6972361ad77dc410f08b25137ae985a4933722cf
Bryan <bknouse@u.rochester.edu>
2013-10-02 18:37:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mfield::Application.config.secret_key_base = '7e8fc15804479e352974380b677ca6a98b4ff18cd7e84da5c5a45f1a0f6849cb187da0f22bf2c336690fcda4877000ed937ae5b4f63d07fdebae535ee35de11a'
